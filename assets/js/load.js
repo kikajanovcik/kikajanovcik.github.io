0cf9fcdae7d3eedc3f21f4ee24810ee258e7098b
kikajanovcik <kristinajanovcikova@gmail.com>
2022-05-11 21:02:10 +0200

    Smooth transition

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,12 +1,18 @@
 window.onload = function() {
-    let element = document.getElementsByClassName("page-header-index")[0];
-    let previous = document.referrer !== '' ? new URL(document.referrer).pathname : '';
+    const header = document.getElementsByClassName("page-header-index")[0];
+    const div = header.querySelector('.banner-index');
+
+    const previous = document.referrer !== '' ? new URL(document.referrer).pathname : '';
     
     if (previous === "/" || previous === "/index") {
-        element.classList.remove("page-header-index");
-        element.classList.add("page-header-transitioning");
+        header.classList.remove("page-header-index");
+        header.classList.add("page-header-transitioning");
+
+        div.classList.remove("banner-index");
+        div.classList.add("banner");
     } else {
-        element.classList.remove("page-header-index");
-        element.classList.add("page-header");
+        header.classList.remove("page-header-index");
+        header.classList.add("page-header");
+        div.classList.remove("banner-index");
     }
 };
