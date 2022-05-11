842723eaad54c74fe80d879afd9cbc0be095fd90
kikajanovcik <kristinajanovcikova@gmail.com>
2022-05-11 17:10:58 +0200

    Slight design update

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,7 +1,7 @@
 window.onload = function() {
-    let previous = new URL(document.referrer).pathname;
     let element = document.getElementsByClassName("page-header-index")[0];
-
+    let previous = document.referrer !== '' ? new URL(document.referrer).pathname : '';
+    
     if (previous === "/" || previous === "/index") {
         element.classList.remove("page-header-index");
         element.classList.add("page-header-transitioning");
